cf1a2efd1930f0e12f8c32ee795eff0bfba57246
AnjRand <anjaharimbol@gmail.com>
2023-05-19 14:23:04 +0300

    First commit

---
+++ Pro_Sante_Connect/Controllers/ProSanteConnect.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pro_Sante_Connect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProSanteConnect : ControllerBase
+    {
+        // GET api/<ProSanteConnect>/Authorization
+        [HttpGet("Authorization/{id}")]
+        public string Authorization(int id)
+        {
+            return "value";
+        }
+        
+        // POST api/<ProSanteConnect>/Token
+        [HttpPost("Token/{id}")]
+        public void Token([FromBody] string value)
+        {
+        }
+
+        // GET api/<ProSanteConnect>/UserInfo
+        [HttpGet("UserInfo/{id}")]
+        public string UserInfo(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProSanteConnect>/RefreshToken
+        [HttpPost("RefreshToken/{id}")]
+        public void RefreshToken([FromBody] string value)
+        {
+        }
+    }
+}
